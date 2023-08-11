9ca0e11d9ce5c16d9d85c0338ec2547af96eb264
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-08-11 10:39:56 -0300

    Randomizar Bolas

--- Assets/Scripts/GeradorDeObjetos.cs
+++ Assets/Scripts/GeradorDeObjetos.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class GeradorDeObjetos : MonoBehaviour
 {
-    public GameObject BolasDeBoliche;
+    
+    public List<GameObject> BolasDeBoliche;
+    public float tempo = 0;
 
     void Update()
     {
-        Instantiate(BolasDeBoliche, transform.position, Quaternion.identity);
+        tempo += Time.deltaTime;
+        if(tempo > 0.5f){
+            tempo = 0;
+            //RandomizarPosição
+            float posX = Random.Range(-2, 2);
+            Vector3 novaPos = new Vector3(posX, 6, 0);
+            //Randomizar o Objeto
+            int indice = Random.Range(0, 4);
+            GameObject novaBolinha = Instantiate(BolasDeBoliche[indice], novaPos, Quaternion.identity);
+            Destroy(novaBolinha, 2f);
+        }
+        
     }
 }
