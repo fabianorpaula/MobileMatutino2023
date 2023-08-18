cbad8d5f4af400d68958340c5cb3d7bb35d85149
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-08-18 11:13:55 -0300

    Botão

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class Jogador : MonoBehaviour
 {
     public bool botaoAtivado = false;
+    public float tempo;
+    public bool podeMover = false;
     
     void Update()
     {
+        //Controle de Tempo
+        tempo += Time.deltaTime;
+        if(tempo> 0.01f)
+        {
+            podeMover = true;
+            tempo = 0;
+        }
+        else
+        {
+            podeMover = false;
+        }
+
         if(botaoAtivado == false)
         {
             MoverDedo();
@@ -18,24 +32,41 @@ public class Jogador : MonoBehaviour
 
     void MoverDedo()
     {
-        if (Input.GetMouseButton(0))
+        if (podeMover == true)
         {
-            //Posição do Mouse
-            Vector3 destino = Input.mousePosition;
-            //Corrigir Posição
-            Vector3 destinoCorrigido = Camera.main.ScreenToWorldPoint(destino);
-            //Travar em X e Y
-            Vector3 destinoFinal = new Vector3(destinoCorrigido.x, -4 + 0.5f, 0);
-            //Jogador Segue
-            transform.position = Vector3.MoveTowards(transform.position, destinoFinal, 0.1f);
+            if (Input.GetMouseButton(0))
+            {
+                //Posição do Mouse
+                Vector3 destino = Input.mousePosition;
+                //Corrigir Posição
+                Vector3 destinoCorrigido = Camera.main.ScreenToWorldPoint(destino);
+                //Travar em X e Y
+                Vector3 destinoFinal = new Vector3(destinoCorrigido.x, -4 + 0.5f, 0);
+                //Jogador Segue
+                transform.position = Vector3.MoveTowards(transform.position, destinoFinal, 0.01f);
+            }
         }
     }
 
     public void MoverE()
     {
-        Vector3 destinoFinal = new Vector3(-2f, -4 + 0.5f, 0);
-        //Jogador Segue
-        transform.position = Vector3.MoveTowards(transform.position, destinoFinal, 0.01f);
+        if (podeMover == true)
+        {
+            Vector3 destinoFinal = new Vector3(-2f, -4 + 0.5f, 0);
+            //Jogador Segue
+            transform.position = Vector3.MoveTowards(transform.position, destinoFinal, 0.01f);
+
+        }
+    }
+
+    public void MoverD()
+    {
+        if (podeMover == true)
+        {
+            Vector3 destinoFinal = new Vector3(2f, -4 + 0.5f, 0);
+            //Jogador Segue
+            transform.position = Vector3.MoveTowards(transform.position, destinoFinal, 0.01f);
+        }
     }
 
 }
